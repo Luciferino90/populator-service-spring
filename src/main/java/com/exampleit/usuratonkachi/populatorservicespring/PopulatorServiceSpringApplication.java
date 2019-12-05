4896ce8dbd0931067f1392dab9e97ca81eabb576
Luca Fanciullini <luca.fanciullini@gmail.com>
2019-12-05 21:32:13 +0100

    First commit

---
+++ src/main/java/com/exampleit/usuratonkachi/populatorservicespring/PopulatorServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package com.exampleit.usuratonkachi.populatorservicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopulatorServiceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopulatorServiceSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exampleit/usuratonkachi/populatorservicespring/PopulatorServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleit.usuratonkachi.populatorservicespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopulatorServiceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
